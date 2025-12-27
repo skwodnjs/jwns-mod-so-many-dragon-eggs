7b18e8e589e47749996f661504012fc0f7e1925f
나재원 <najaewon@najaewon-ui-MacBookPro.local>
2025-12-27 20:22:15 +0900

    basic setup

---
+++ src/main/java/net/jwn/jwnssomanydragoneggs/events/ModEvents.java
@@ -0,0 +1,9 @@
+package net.jwn.jwnssomanydragoneggs.events;
+
+import net.jwn.jwnssomanydragoneggs.JWNsDragonEggMod;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = JWNsDragonEggMod.MOD_ID)
+public class ModEvents {
+
+}
